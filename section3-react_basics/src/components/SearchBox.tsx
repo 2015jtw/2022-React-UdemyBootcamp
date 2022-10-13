a144ddb93021704d07df231e52aff71bc9f203c9
Josh Worden <2015JTW@gmail.com>
2022-10-13 15:25:10 -0400

    adding TS

--- section3-react_basics/src/components/SearchBox.tsx
+++ section3-react_basics/src/components/SearchBox.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import '../SearchBox.css'
 
+const name: string = '1234567';
+
+interface ISearchBoxProperties extends IChangeHandlerProps{
+    classname: string;
+    placeholder?: string
+}
+
+interface IChangeHandlerProps{
+    onChangeHandler: (a: string) => void
+}
+
 const SearchBox = ({className, placeholder, onChangeHandler}) => {
 
     return (
